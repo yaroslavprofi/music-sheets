5ebf066765ea388531b4d13809221ec58c3e3d12
yaroslavprofi <yaroslavprofi@yandex.ru>
2021-04-06 20:18:12 +0300

    hosted on aws, minor improvements

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,18 @@ key=$3
 
 # 1. kill existing servers and clean old version
 ssh -i $key $hostUser@$host "sudo killall java"
-#ssh -i $key $hostUser@$host "rm -rf ~/app"
-#ssh -i $key $hostUser@$host "mkdir -p ~/app"
+ssh -i $key $hostUser@$host "rm -rf ~/app"
+ssh -i $key $hostUser@$host "mkdir -p ~/app"
 
 # 2. copy bundle to host
-#scp -i $key -r ./build/dist $hostUser@$host:~/app/
+scp -i $key -r ./build/dist $hostUser@$host:~/app/
 
 # 3. start server
 executeCommand="cd ~/app/dist; sudo bash server/server-0.1.1/bin/server"
 ssh -i $key $hostUser@$host "$executeCommand"
 
 # (4. kill after closing console, you can comment that and it still will be working)
-ssh -i $key $hostUser@$host "sudo killall java"
+#ssh -i $key $hostUser@$host "sudo killall java"
 
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
